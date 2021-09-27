bd46c0fe13b98d934f7388f9b7d535c6693d5253
rassafel <rassafel@gmail.com>
2021-09-27 10:33:56 +0400

    Uploaded lecture on arrays.

---
+++ src/main/java/ru/nshi/learn/lecture2/ArraysLecture.java
@@ -0,0 +1,63 @@
+package ru.nshi.learn.lecture2;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * @author rassafel
+ */
+public class ArraysLecture {
+    public static void main(String[] args) {
+        ArraysLecture application = new ArraysLecture();
+        int[] array = application.createArray(10);
+        array = application.fillArrayByRandom(array);
+        application.printArray(array);
+
+        System.out.println();
+
+        int[][] matrix = new int[5][];
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = application.createArray(i);
+        }
+        application.printArray(matrix);
+    }
+
+    public int[] createArray(int length) {
+        int[] array = new int[length];
+        return array;
+    }
+
+    public int[] fillArrayByScanner(int[] array) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Input number: ");
+            array[i] = sc.nextInt();
+        }
+        return array;
+    }
+
+    public int[] fillArrayByRandom(int[] array) {
+        Random rnd = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd.nextInt(90) + 10;
+        }
+        return array;
+    }
+
+    public void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if(i+1 < array.length) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public void printArray(int[][] array) {
+        for(int[] row: array) {
+            printArray(row);
+        }
+    }
+}
