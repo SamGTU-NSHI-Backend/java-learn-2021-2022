9bea956e6b5cbe383873c53b80a6da1d6adb3c6e
rassafel <rassafel@gmail.com>
2021-10-05 09:32:16 +0400

    Created indexOf method.

--- src/main/java/ru/nshi/learn/lecture3/AlgorithmsLecture.java
+++ src/main/java/ru/nshi/learn/lecture3/AlgorithmsLecture.java
@@ -1,7 +1,36 @@
 package ru.nshi.learn.lecture3;
 
+import ru.nshi.learn.lecture2.ArraysLecture;
+
 /**
  * @author rassafel
  */
 public class AlgorithmsLecture {
+    public static void main(String[] args) {
+        ArraysLecture arraysLecture = new ArraysLecture();
+        int[] array = arraysLecture.createArray(5);
+        arraysLecture.fillArrayByRandom(array);
+        arraysLecture.printArray(array);
+
+        AlgorithmsLecture algorithmsLecture = new AlgorithmsLecture();
+        int index = algorithmsLecture.indexOf(array, 3);
+        System.out.println("index = " + index);
+    }
+
+    /**
+     * Find first match by value.
+     *
+     * @param array
+     * @param value
+     * @return index in array
+     * if value not exist returns -1
+     */
+    public int indexOf(int[] array, int value) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == value) {
+                return index;
+            }
+        }
+        return -1;
+    }
 }
