734671dd2fed357993714f53a516731ee0fe1f2b
rassafel <rassafel@gmail.com>
2021-10-11 10:36:34 +0400

    Refactored implementation.

--- src/main/java/ru/nshi/learn/lecture4/Task4.java
+++ src/main/java/ru/nshi/learn/lecture4/Task4.java
@@ -7,25 +7,30 @@ import ru.nshi.learn.lecture2.ArraysLecture;
  */
 public class Task4 {
     public static void main(String[] args) {
-        ArraysLecture arraysLecture = new ArraysLecture();
         Task4 task4 = new Task4();
 
-        int[][] points = {
-            {1, 1},
-            {1, 4},
-            {4, 4},
-            {4, 1}
+        Point[] points = {
+            new Point(1, 1),
+            new Point(1, 4),
+            new Point(4, 4),
+            new Point(4, 1)
         };
 
         System.out.println("Исходный массив: ");
-        arraysLecture.printArray(points);
+        task4.printPoints(points);
         System.out.println();
 
         double square = task4.square(points);
         System.out.println("Площадь квадрата равна: " + square);
     }
 
-    public double square(int[][] points) {
+    public void printPoints(Point[] points) {
+        for (Point point : points) {
+            System.out.printf("Point: x=%d, y=%d\n", point.x, point.y);
+        }
+    }
+
+    public double square(Point[] points) {
         double[] lengths = calculateLengths(points);
         if(!isEquals(lengths)) {
             System.err.println("Данный объект не является квадратом");
@@ -45,23 +50,19 @@ public class Task4 {
         return true;
     }
 
-    public double[] calculateLengths(int[][] points) {
+    public double[] calculateLengths(Point[] points) {
         double[] lengths = new double[points.length];
         for (int i = 0; i < points.length; i++) {
-            int[] pointA = points[i];
-            int[] pointB = points[(i + 1) % points.length];
+            Point pointA = points[i];
+            Point pointB = points[(i + 1) % points.length];
             lengths[i] = calculateLength(pointA, pointB);
         }
         return lengths;
     }
 
-    public double calculateLength(int[] pointA, int[] pointB) {
-        if(pointA.length != 2 || pointB.length != 2) {
-            System.err.println("Точка не содержит двух значений");
-            return -1;
-        }
-        int diffX = pointA[0] - pointB[0];
-        int diffY = pointA[1] - pointB[1];
+    public double calculateLength(Point pointA, Point pointB) {
+        int diffX = pointA.x - pointB.x;
+        int diffY = pointA.y - pointB.y;
         double powDiffX = Math.pow(diffX, 2);
         double powDiffY = Math.pow(diffY, 2);
         return Math.sqrt(powDiffX + powDiffY);
