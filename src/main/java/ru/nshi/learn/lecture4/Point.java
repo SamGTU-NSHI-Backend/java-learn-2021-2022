c068995393ba134535df5e523ccdd7e0d0551678
rassafel <rassafel@gmail.com>
2021-10-11 10:34:58 +0400

    Created Point model.

---
+++ src/main/java/ru/nshi/learn/lecture4/Point.java
@@ -0,0 +1,14 @@
+package ru.nshi.learn.lecture4;
+
+/**
+ * @author rassafel
+ */
+public class Point {
+    public final int x;
+    public final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
