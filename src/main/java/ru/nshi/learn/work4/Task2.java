1f92b7783cdf97c02099056eaba9ebdf572bb958
Monenvoy <goldseeker68191@gmail.com>
2021-11-07 19:20:59 +0400

    Added the solution to the 2nd Task

--- src/main/java/ru/nshi/learn/work4/Task2.java
+++ src/main/java/ru/nshi/learn/work4/Task2.java
@@ -1,2 +1,21 @@
-package ru.nshi.learn.work4;public class Task2 {
+package ru.nshi.learn.work4;
+
+public class Task2 {
+    public static void main (String[] args) {
+        Task2 task2 = new Task2();
+        String string = "ФSва12ЕE_3Q";
+        String filtered_string = task2.filterUpper(string);
+        System.out.println(filtered_string);
+    }
+    public String filterUpper(String string) {
+        String filtered_string = 
+            string
+            .chars()
+            .mapToObj(c -> (char) c)
+            .filter(chr -> Character.isUpperCase(chr))
+            .map(c -> c.toString())
+            .reduce("", (String str1, String str2) -> str1 + str2);
+
+        return filtered_string;
+    }
 }
