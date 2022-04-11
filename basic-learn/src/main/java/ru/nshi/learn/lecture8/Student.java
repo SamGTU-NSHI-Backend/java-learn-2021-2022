b2f2ffe0f58ada3d7e663d4c11d6a34feddb1ed8
rassafel <rassafel@gmail.com>
2022-04-11 10:01:35 +0400

    Add comparator, getters, setters, constructor and override equals, hashcode and toString.

--- basic-learn/src/main/java/ru/nshi/learn/lecture8/Student.java
+++ basic-learn/src/main/java/ru/nshi/learn/lecture8/Student.java
@@ -1,11 +1,85 @@
 package ru.nshi.learn.lecture8;
 
+import java.util.Comparator;
+
 /**
  * @author rassafel
  */
-public class Student {
+public class Student implements Comparable<Student> {
+    public static final Comparator<Student> COMPARATOR_BY_COURSE =
+        Comparator.comparingInt(Student::getCourse);
+
     private int id;
     private String firstName;
     private int course;
     private Faculity faculity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    public Student(int id, String firstName, int course, Faculity faculity) {
+        this.id = id;
+        this.firstName = firstName;
+        this.course = course;
+        this.faculity = faculity;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", course=" + course +
+            ", faculity=" + faculity +
+            '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public Faculity getFaculity() {
+        return faculity;
+    }
+
+    public void setFaculity(Faculity faculity) {
+        this.faculity = faculity;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return COMPARATOR_BY_COURSE.compare(this, o);
+    }
 }
